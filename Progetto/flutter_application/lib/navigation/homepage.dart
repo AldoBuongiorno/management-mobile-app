a0af8c399abfce88486e668f55b87b3b3e86bc4a
CarmineVardy <150947253+CarmineVardy@users.noreply.github.com>
2024-06-02 12:02:09 +0200

    altro

--- Progetto/flutter_application/lib/navigation/homepage.dart
+++ Progetto/flutter_application/lib/navigation/homepage.dart
@@ -134,86 +134,109 @@ class HomePageScreen extends StatelessWidget {
             : ProjectList.projectOnHomepageNumber*/
           ,
           itemBuilder: (context, index) {
-            return FutureBuilder<List<Member>>(
-              future: DatabaseHelper.instance
-                  .getMembersByTeam(testList[index].getName()),
-              builder: (context, membersSnapshot) {
-                if (membersSnapshot.connectionState ==
-                    ConnectionState.waiting) {
-                  return CircularProgressIndicator(); // Visualizza un indicatore di caricamento durante il caricamento dei membri del team
-                } else if (membersSnapshot.hasError) {
-                  return Text(
-                      'Errore: ${membersSnapshot.error}'); // Gestisci eventuali errori durante il caricamento dei membri del team
-                } else {
-                  return ExpandableTeamTile(
-                      testList[index].getName(), membersSnapshot.data!, index);
-                }
-              },
-            );
+Team testItem = testList[index];
+          return ExpandableTeamTile(testItem.getName(), index);
+
+            
           });
     }
   }
 }
 
-
 class ExpandableTeamTile extends StatefulWidget {
-  ExpandableTeamTile(this.teamName,this.memberList, this.index, {super.key});
-  String teamName;
-  List<Member> memberList;
-  int index;
+  final String teamName;
+  final int index;
+
+  const ExpandableTeamTile(
+    this.teamName,
+    this.index,
+  {super.key});
 
   @override
-  State<ExpandableTeamTile > createState() => _ExpandableTeamTileState();
+  State<ExpandableTeamTile> createState() => _ExpandableTeamTileState();
 }
 
+class _ExpandableTeamTileState extends State<ExpandableTeamTile> {
+  late Future<List<Member>> _membersFuture;
 
-class _ExpandableTeamTileState extends State<ExpandableTeamTile > {
-  bool _customTileExpanded = false;
+  @override
+  void initState() {
+    super.initState();
+    _membersFuture = DatabaseHelper.instance.getMembersByTeam(widget.teamName);
+  }
 
   @override
   Widget build(BuildContext context) {
-    //teamList[widget.index].members = memberList;
     return Theme(
-        data: ThemeData().copyWith(
-          dividerColor: Colors.transparent,
+      data: ThemeData().copyWith(
+        dividerColor: Colors.transparent,
+      ),
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 5),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25),
         ),
-        child: Container(
-            margin: const EdgeInsets.symmetric(vertical: 5),
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(25)),
-            child: Column(
-              children: <Widget>[
-                ExpansionTile(
-                  iconColor: Colors.lightBlue,
-                  collapsedIconColor: Colors.pink,
-                  expandedAlignment: Alignment.centerLeft,
-
-                  title: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          widget.teamName,
-                          style: const TextStyle(
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                            ("(${widget.memberList.length} membri)"),
-                            style: const TextStyle(
-                                fontFamily: 'Poppins', fontSize: 14))
-                      ]),
-                  //subtitle: Text('Trailing expansion arrow icon'),
-                  children: [
-                    for (Member member 
-                        in widget.memberList)
-                      Container(
-                          alignment: Alignment.centerLeft,
-                          margin: const EdgeInsets.only(left: 30, bottom: 5),
-                          child: Text('${member.name} ${member.surname}'))
-                  ]
+        child: ExpansionTile(
+          iconColor: Colors.lightBlue,
+          collapsedIconColor: Colors.pink,
+          expandedAlignment: Alignment.centerLeft,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                widget.teamName,
+                style: const TextStyle(
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.bold,
                 ),
-              ],
-            )));
+              ),
+              FutureBuilder<List<Member>>(
+                future: _membersFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Errore: ${snapshot.error}'));
+                  } else if (snapshot.hasData) {
+                    return Text(
+                      "(${snapshot.data!.length} membri)",
+                      style: const TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 14,
+                      ),
+                    );
+                  } else {
+                    return const SizedBox();
+                  }
+                },
+              ),
+            ],
+          ),
+          children: [
+            FutureBuilder<List<Member>>(
+              future: _membersFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+                  return Column(
+                    children: snapshot.data!.map((member) {
+                      return Container(
+                        alignment: Alignment.centerLeft,
+                        margin: const EdgeInsets.only(left: 30, bottom: 5),
+                        child: Text('${member.name} ${member.surname}'),
+                      );
+                    }).toList(),
+                  );
+                } else {
+                  return const SizedBox();
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
-
 }
+
+
