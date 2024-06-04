1f59444cc5251e42c4b7d706a508743f5141e511
Aldo0241 <aldobuongiorno2002@gmail.com>
2024-06-04 22:56:45 +0200

    Formattazione Projects Page

--- Progetto/flutter_application/lib/navigation/projects_page.dart
+++ Progetto/flutter_application/lib/navigation/projects_page.dart
@@ -23,7 +23,7 @@ class _ProjectScreenState extends State<ProjectScreen> {
   void initState() {
     super.initState();
     _loadProjects();
-     filterProjectListController.addListener(() {
+    filterProjectListController.addListener(() {
       _filterProjects(filterProjectListController.text);
     });
   }
@@ -37,8 +37,8 @@ class _ProjectScreenState extends State<ProjectScreen> {
   Future<void> _loadProjects() async {
     final projects = await DatabaseHelper.instance.getProjects();
     setState(() {
-      this.projectList = projects;
-      this.filteredList = projects;
+      projectList = projects;
+      filteredList = projects;
     });
   }
 
@@ -81,112 +81,96 @@ class _ProjectScreenState extends State<ProjectScreen> {
           ),
           const SizedBox(height: 20),
           Expanded(
-            child: filteredList.isNotEmpty
-                ? ListView.builder(
-                    physics: const AlwaysScrollableScrollPhysics()
-                        .applyTo(const BouncingScrollPhysics()),
-                    itemCount: filteredList.length,
-                    itemBuilder: (context, index) {
-                      return GestureDetector(
-                        onTap: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ProjectRoute(filteredList[index]),
-                          ),
-                        ).then((_) => _loadProjects()),
-                        child: Container(
-                          height: 200,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: filteredList[index].thumbnail,
-                              fit: BoxFit.cover,
+            child: filteredList.isNotEmpty ? ListView.builder(
+              physics: const AlwaysScrollableScrollPhysics().applyTo(const BouncingScrollPhysics()),
+              itemCount: filteredList.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ProjectRoute(filteredList[index])),
+                  ).then((_) => _loadProjects()),
+                  child: Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: filteredList[index].thumbnail,
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: const BorderRadius.all(Radius.circular(20)),
+                    ),
+                    padding: EdgeInsets.zero,
+                    margin: const EdgeInsets.symmetric(vertical: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            statusCheck(filteredList[index]),
+                            IconButton(
+                              onPressed: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => EditProjectScreen(
+                                  project: filteredList[index],
+                                )),
+                              ).then((_) => _loadProjects()),
+                              icon: const Icon(
+                                Icons.draw,
+                                color: Colors.white,
+                              ),
                             ),
-                            borderRadius: const BorderRadius.all(Radius.circular(20)),
-                          ),
-                          padding: EdgeInsets.zero,
-                          margin: const EdgeInsets.symmetric(vertical: 10),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  statusCheck(filteredList[index]),
-                                  IconButton(
-                                    onPressed: () => Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => EditProjectScreen(
-                                          project: filteredList[index],
-                                        ),
-                                      ),
-                                    ).then((_) => _loadProjects()),
-                                    icon: const Icon(
-                                      Icons.draw,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Column(
-                                children: [
-                                  Row(
-                                    children: [
-                                      const SizedBox(width: 12),
-                                      Container(
-                                        margin: const EdgeInsets.only(bottom: 10),
-                                        decoration: BoxDecoration(
-                                          color: Colors.white,
-                                          borderRadius: BorderRadius.circular(10),
-                                        ),
-                                        child: Padding(
-                                          padding: const EdgeInsets.symmetric(horizontal: 8),
-                                          child: teamCheck(filteredList[index]),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  BlurredBox(
-                                    borderRadius: const BorderRadius.vertical(
-                                      bottom: Radius.circular(20),
-                                    ),
-                                    sigma: 15,
-                                    child: Container(
-                                      padding: const EdgeInsets.symmetric(vertical: 3),
-                                      decoration: const BoxDecoration(
-                                        color: Color.fromARGB(100, 0, 0, 0),
-                                      ),
-                                      child: Row(
-                                        children: [
-                                          const SizedBox(width: 15),
-                                          getProjectName(filteredList[index]),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
+                          ],
                         ),
-                      );
-                    },
-                  )
-                : const Center(child: Text("Nessun progetto trovato")),
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                const SizedBox(width: 12),
+                                Container(
+                                  margin: const EdgeInsets.only(bottom: 10),
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                                    child: teamCheck(filteredList[index]),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            BlurredBox(
+                              borderRadius: const BorderRadius.vertical(
+                                bottom: Radius.circular(20),
+                              ),
+                              sigma: 15,
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(vertical: 3),
+                                decoration: const BoxDecoration(
+                                  color: Color.fromARGB(100, 0, 0, 0),
+                                ),
+                                child: Row(
+                                  children: [
+                                    const SizedBox(width: 15),
+                                    getProjectName(filteredList[index]),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ) : const Center(child: Text("Nessun progetto trovato")),
           ),
         ],
       ),
     );
   }
 }
-
-
-
-/*class ProjectContainer extends StatefulWidget {
-  ProjectContainer({super.key, required this.projectsList});
-  List<Project> projectsList;
-
-  State<ProjectContainer>
-}*/
 
 
