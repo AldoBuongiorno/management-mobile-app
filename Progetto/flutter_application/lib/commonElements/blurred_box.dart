c5acafba91409204bd96d83a5cf52ecc2adb1323
Aldo0241 <aldobuongiorno2002@gmail.com>
2024-06-05 13:14:32 +0200

    Formattazione Blurred Box

--- Progetto/flutter_application/lib/commonElements/blurred_box.dart
+++ Progetto/flutter_application/lib/commonElements/blurred_box.dart
@@ -3,15 +3,15 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class BlurredBox extends StatelessWidget {
-  BlurredBox({required this.child, required this.borderRadius, required this.sigma});
-  late final BorderRadius borderRadius;
-  late final Widget child;
-  late final double sigma;
+  const BlurredBox({super.key, required this.child, required this.borderRadius, required this.sigma});
+  final BorderRadius borderRadius;
+  final Widget child;
+  final double sigma;
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-        borderRadius: borderRadius,
-        child: BackdropFilter(filter: ImageFilter.blur(sigmaX: sigma, sigmaY: sigma), child: child),
-        );
+      borderRadius: borderRadius,
+      child: BackdropFilter(filter: ImageFilter.blur(sigmaX: sigma, sigmaY: sigma), child: child),
+    );
   }
 }
