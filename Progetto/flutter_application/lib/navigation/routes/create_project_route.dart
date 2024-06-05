cff4ed1168924a8a68a5b9dfdb1e73ceea95b3a1
Aldo0241 <aldobuongiorno2002@gmail.com>
2024-06-05 12:04:18 +0200

    Formattazione Create Project Route

--- Progetto/flutter_application/lib/navigation/routes/create_project_route.dart
+++ Progetto/flutter_application/lib/navigation/routes/create_project_route.dart
@@ -32,30 +32,29 @@ class _CreateProjectScreenState extends State<CreateProjectScreen> {
   void initState() {
     super.initState();
 
-  grid = SelectableThumbnailGrid(
+    grid = SelectableThumbnailGrid(
       selectedThumbnail: 0,
       list: Thumbnail.projectThumbnails
     );
     dontSetStatePlease = FutureBuilder(
-                    future: DatabaseHelper.instance.getTeams(),
-                    builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const SizedBox();
-                      } else if (snapshot.hasError) {
-                        return Text('Error: ${snapshot.error}');
-                      } else {
-                        chips = SelectableTeamsList(
-                          teamsList: snapshot.data!,
-                          selectedTeam: 0,
-                        );
-                        return SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          child: Row(children: [chips]),
-                        );
-                      }
-                    }
-                  );
-
+      future: DatabaseHelper.instance.getTeams(),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const SizedBox();
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          chips = SelectableTeamsList(
+            teamsList: snapshot.data!,
+            selectedTeam: 0,
+          );
+          return SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(children: [chips]),
+          );
+        }
+      }
+    );
   }
 
   @override
@@ -70,243 +69,178 @@ class _CreateProjectScreenState extends State<CreateProjectScreen> {
     
     Project projectItem;
     return Container(
-        margin: EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal:
-                MediaQuery.of(context).orientation == Orientation.portrait
-                    ? 20
-                    : 100),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+      margin: EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: MediaQuery.of(context).orientation == Orientation.portrait ? 20 : 100
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end, 
+        children: [
           const SizedBox(height: 20),
-          Row(children: [
-            //SizedBox(width: 5),
-            CustomHeadingTitle(titleText: "Nome"),
-          ]),
+          Row(children: [CustomHeadingTitle(titleText: "Nome")]),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
             child: BlurredBox(
-                borderRadius: BorderRadius.circular(30),
-                sigma: 15,
-                child: TextField(
-                  maxLength: 50,
-                  style: const TextStyle(color: Colors.white),
-                  controller: projectNameController,
-                  decoration: const InputDecoration(
-                      counterText: '',
-                      contentPadding:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-                      filled: true,
-                      fillColor: Color.fromARGB(100, 0, 0, 0),
-                      border: OutlineInputBorder(
-                          //borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide.none),
-                      hintText: 'Inserisci il nome del progetto',
-                      hintStyle:
-                          TextStyle(color: Color.fromARGB(255, 192, 192, 192))),
-                )),
+              borderRadius: BorderRadius.circular(30),
+              sigma: 15,
+              child: TextField(
+                maxLength: 50,
+                style: const TextStyle(color: Colors.white),
+                controller: projectNameController,
+                decoration: const InputDecoration(
+                  counterText: '',
+                  contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                  filled: true,
+                  fillColor: Color.fromARGB(100, 0, 0, 0),
+                  border: OutlineInputBorder(borderSide: BorderSide.none),
+                  hintText: 'Inserisci il nome del progetto',
+                  hintStyle: TextStyle(color: Color.fromARGB(255, 192, 192, 192))
+                ),
+              )
+            ),
           ),
           const SizedBox(height: 5),
-          Row(children: [
-            //SizedBox(width: 25),
-            CustomHeadingTitle(titleText: "Descrizione"),
-          ]),
+          Row(children: [CustomHeadingTitle(titleText: "Descrizione")]),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
             child: BlurredBox(
-                borderRadius: BorderRadius.circular(10),
-                sigma: 15,
-                child: TextField(
-                  maxLength: 500,
-                  style: const TextStyle(color: Colors.white),
-                  maxLines: 5,
-                  controller: projectDescriptionController,
-                  decoration: const InputDecoration(
-                      counterText: '',
-                      contentPadding:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-                      filled: true,
-                      fillColor: Color.fromARGB(100, 0, 0, 0),
-                      border: OutlineInputBorder(borderSide: BorderSide.none),
-                      hintText: 'Questo progetto si pone l\' obiettivo di...',
-                      hintStyle:
-                          TextStyle(color: Color.fromARGB(255, 192, 192, 192))),
-                )),
+              borderRadius: BorderRadius.circular(10),
+              sigma: 15,
+              child: TextField(
+                maxLength: 500,
+                style: const TextStyle(color: Colors.white),
+                maxLines: 5,
+                controller: projectDescriptionController,
+                decoration: const InputDecoration(
+                  counterText: '',
+                  contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                  filled: true,
+                  fillColor: Color.fromARGB(100, 0, 0, 0),
+                  border: OutlineInputBorder(borderSide: BorderSide.none),
+                  hintText: 'Questo progetto si pone l\' obiettivo di...',
+                  hintStyle: TextStyle(color: Color.fromARGB(255, 192, 192, 192))
+                ),
+              )
+            ),
           ),
           const SizedBox(height: 5),
-          Row(children: [
-            //SizedBox(width: 25),
-            CustomHeadingTitle(titleText: "Team"),
-          ]),
+          Row(children: [CustomHeadingTitle(titleText: "Team")]),
           const SizedBox(height: 5),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 0),
             child: dontSetStatePlease
           ),
           const SizedBox(height: 5),
-          Row(children: [
-            //SizedBox(width: 25),
-            CustomHeadingTitle(titleText: "Copertina"),
-          ]),
+          Row(children: [CustomHeadingTitle(titleText: "Copertina")]),
           grid,
-          Row(children: [
-            //SizedBox(width: 25),
-            CustomHeadingTitle(titleText: "Task"),
-          ]),
+          Row(children: [CustomHeadingTitle(titleText: "Task")]),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
             child: BlurredBox(
-                borderRadius: BorderRadius.circular(10),
-                sigma: 15,
-                child: TextField(
-                  style: const TextStyle(color: Colors.white),
-                  controller: taskInputController,
-                  decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                          onPressed: () async {
-                            if (taskInputController.text.isNotEmpty) {
-                              if (!cTasks.any((task) =>
-                                  task.name == taskInputController.text)) {
-                                cTasks
-                                    .add(Task(name: taskInputController.text));
-                              }
-                              ;
-                            }
-
-                            taskInputController.clear();
-                            setState(() {});
-                          },
-                          icon: const Icon(
-                            Icons.add,
-                            color: Colors.white,
-                          )),
-                      contentPadding: const EdgeInsets.symmetric(
-                          vertical: 15, horizontal: 15),
-                      filled: true,
-                      fillColor: const Color.fromARGB(100, 0, 0, 0),
-                      border: const OutlineInputBorder(
-                          //borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide.none),
-                      hintText: 'Inserisci una task',
-                      hintStyle: const TextStyle(
-                          color: Color.fromARGB(255, 214, 214, 214))),
-                )),
-
-            //TextButton(onPressed: () { }, child: Icon(Icons.add))
+              borderRadius: BorderRadius.circular(10),
+              sigma: 15,
+              child: TextField(
+                style: const TextStyle(color: Colors.white),
+                controller: taskInputController,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    onPressed: () async {
+                      if (taskInputController.text.isNotEmpty) {
+                        if (!cTasks.any((task) => task.name == taskInputController.text)) {
+                          cTasks.add(Task(name: taskInputController.text));
+                        }
+                      }
+                      taskInputController.clear();
+                      setState(() {});
+                    },
+                    icon: const Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    )
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                  filled: true,
+                  fillColor: const Color.fromARGB(100, 0, 0, 0),
+                  border: const OutlineInputBorder(borderSide: BorderSide.none),
+                  hintText: 'Inserisci una task',
+                  hintStyle: const TextStyle(color: Color.fromARGB(255, 214, 214, 214))
+                ),
+              )
+            ),
           ),
           taskCheckboxList,
           ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white, backgroundColor: Colors.pink),
-              onPressed: () async {
-                List<Team> teamsList = await DatabaseHelper.instance.getTeams();
-                projectNameController.text.isEmpty ||
-                        projectDescriptionController.text.isEmpty ||
-                        teamsList.isEmpty
-                    ? showDialog<String>(
-                        context: context,
-                        builder: (BuildContext context) => AlertDialog(
-                          title: const Text('Errore'),
-                          content: const Text(
-                              "Il progetto non può avere nome o descrizione vuoto.\nInoltre devono essere possibile assegnare un team al progetto."),
-                          actions: <Widget>[
-                            TextButton(
-                              onPressed: () => Navigator.pop(context, 'Ok'),
-                              child: const Text('Ok'),
-                            ),
-                          ],
-                        ),
-                      )
-                    : {
-                        projectItem = Project(
-                            name: projectNameController.text,
-                            description: projectDescriptionController.text,
-                            status: "Attivo",
-                            team: teamsList[chips.selectedTeam],
-                            thumbnail: Thumbnail
-                                .projectThumbnails[grid.selectedThumbnail]),
-                        DatabaseHelper.instance.insertProject(projectItem),
-
-                        for (Task task in cTasks) task.setProject(projectItem),
-                        for (Task task in cTasks)
-                          DatabaseHelper.instance.insertTask(task),
-
-                        //cTasks = await DatabaseHelper.instance.getTasksByProjectName(projectItem.name),
-                        projectNameController.clear(),
-                        projectDescriptionController.clear(),
-                        grid.selectedThumbnail = 0,
-                        cTasks.clear(), setState(() {}),
-
-                        Navigator.of(context).pop(),
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          padding: EdgeInsets.zero,
-                          elevation: 0,
-                          backgroundColor: Colors.transparent,
-                          content: Container(
-                              color: const Color.fromARGB(156, 0, 0, 0),
-                              child: BlurredBox(
-                                  sigma: 20,
-                                  borderRadius: BorderRadius.zero,
-                                  child: const Column(children: [
-                                    SizedBox(
-                                      height: 10,
-                                    ),
-                                    Text('Progetto creato con successo!'),
-                                    SizedBox(
-                                      height: 10,
-                                    )
-                                  ]))),
-                        ))
-                      };
-              },
-              child: const Row(mainAxisSize: MainAxisSize.min, children: [
-                Icon(Icons.add_task),
-                SizedBox(
-                  width: 5,
+            style: ElevatedButton.styleFrom(foregroundColor: Colors.white, backgroundColor: Colors.pink),
+            onPressed: () async {
+              List<Team> teamsList = await DatabaseHelper.instance.getTeams();
+              projectNameController.text.isEmpty || projectDescriptionController.text.isEmpty ||
+              teamsList.isEmpty ? showDialog<String>(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  title: const Text('Errore'),
+                  content: const Text("Il progetto non può avere nome o descrizione vuoto.\nInoltre devono essere possibile assegnare un team al progetto."),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, 'Ok'),
+                      child: const Text('Ok'),
+                    ),
+                  ],
                 ),
+              ) : {
+                projectItem = Project(
+                  name: projectNameController.text,
+                  description: projectDescriptionController.text,
+                  status: "Attivo",
+                  team: teamsList[chips.selectedTeam],
+                  thumbnail: Thumbnail.projectThumbnails[grid.selectedThumbnail]
+                ),
+                
+                DatabaseHelper.instance.insertProject(projectItem),
+                
+                for (Task task in cTasks) task.setProject(projectItem),
+                for (Task task in cTasks) DatabaseHelper.instance.insertTask(task),
+
+                projectNameController.clear(),
+                projectDescriptionController.clear(),
+                grid.selectedThumbnail = 0,
+                cTasks.clear(), setState(() {}),
+
+                Navigator.of(context).pop(),
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    padding: EdgeInsets.zero,
+                    elevation: 0,
+                    backgroundColor: Colors.transparent,
+                    content: Container(
+                      color: const Color.fromARGB(156, 0, 0, 0),
+                      child: BlurredBox(
+                        sigma: 20,
+                        borderRadius: BorderRadius.zero,
+                        child: const Column(
+                          children: [
+                            SizedBox(height: 10),
+                            Text('Progetto creato con successo!'),
+                            SizedBox(height: 10)
+                          ]
+                        )
+                      )
+                    ),
+                  )
+                )
+              };
+            },
+            child: const Row(
+              mainAxisSize: MainAxisSize.min, 
+              children: [
+                Icon(Icons.add_task),
+                SizedBox(width: 5),
                 Text("Aggiungi progetto")
-              ])),
+              ]
+            )
+          ),
           const SizedBox(height: 30)
-        ]));
+        ]
+      )
+    );
   }
 }
-/*
-class SelectableTeamsList extends StatefulWidget {
-  const SelectableTeamsList({super.key});
-
-  @override
-  State<SelectableTeamsList> createState() => _SelectableTeamsListState();
-}
-
-class _SelectableTeamsListState extends State<SelectableTeamsList> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ProjectList.teamsList.isNotEmpty
-        ? Wrap(
-            spacing: 5.0,
-            children: List<Widget>.generate(
-              ProjectList.teamsList.length,
-              (int index) {
-                return ChoiceChip(
-                  selectedColor: Colors.pink,
-                  iconTheme: const IconThemeData(color: Colors.white),
-                  label: Text(ProjectList.teamsList[index].getName()),
-                  selected: selectedTeam == index,
-                  onSelected: (bool selected) {
-                    setState(() {
-                      selectedTeam = selected ? index : 0;
-                    });
-                  },
-                );
-              },
-            ).toList(),
-          )
-        : const Expanded(
-            child: Text(
-                "Non ci sono team disponibili. Non è possibile creare un progetto senza team."));
-  }
-}*/
