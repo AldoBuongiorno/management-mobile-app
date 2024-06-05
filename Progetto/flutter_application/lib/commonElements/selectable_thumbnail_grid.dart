bef0567d78ec27658c9336a6636ccbff06cd1e68
Aldo0241 <aldobuongiorno2002@gmail.com>
2024-06-05 12:43:28 +0200

    Formattazione Selectable Thumbnail Grid

--- Progetto/flutter_application/lib/commonElements/selectable_thumbnail_grid.dart
+++ Progetto/flutter_application/lib/commonElements/selectable_thumbnail_grid.dart
@@ -3,7 +3,7 @@ import 'blurred_box.dart';
 
 // ignore: must_be_immutable
 class SelectableThumbnailGrid extends StatefulWidget {
-  SelectableThumbnailGrid({this.selectedThumbnail = 0, required this.list });
+  SelectableThumbnailGrid({super.key, this.selectedThumbnail = 0, required this.list });
 
   int selectedThumbnail; List<AssetImage> list;
 
@@ -12,81 +12,72 @@ class SelectableThumbnailGrid extends StatefulWidget {
   }
 
   @override
-  State<SelectableThumbnailGrid> createState() =>
-      _SelectableThumbnailGridState();
+  State<SelectableThumbnailGrid> createState() => _SelectableThumbnailGridState();
 }
 
 class _SelectableThumbnailGridState extends State<SelectableThumbnailGrid> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
-        child: GridView.builder(
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            scrollDirection: Axis.vertical,
-            itemCount: widget.list.length,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount:
-                  MediaQuery.of(context).orientation == Orientation.portrait
-                      ? 2
-                      : 3,
-              childAspectRatio:
-                  2, //MediaQuery.of(context).size.width / (MediaQuery.of(context).size.height / 1.4),
-            ),
-            itemBuilder: (BuildContext context, int index) {
-              return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      widget.selectedThumbnail = index;
-                    });
-                  },
-                  child: Card(
-                      child: Container(
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  image: widget.list[index],
-                                  fit: BoxFit.cover),
-                              borderRadius: BorderRadius.circular(10)),
-                          height: 200,
-                          width: 200,
-                          child: widget.selectedThumbnail == index
-                              ? BlurredBox(
-                                  borderRadius: BorderRadius.circular(10),
-                                  sigma: 2,
-                                  child: Container(
-                                    decoration: const BoxDecoration(
-                                        color: Color.fromARGB(100, 0, 0, 0)),
-                                    child: Center(
-                                      child: ShaderMask(
-                                        blendMode: BlendMode.srcIn,
-                                        shaderCallback: (Rect bounds) =>
-                                            const LinearGradient(
-                                          begin: Alignment.topRight,
-                                          end: Alignment.bottomLeft,
-                                          
-                                          colors: [
-                                            
-                                            Colors.lightBlue,
-                                            Colors.pink,
-                                            Colors.yellow,
-                                            
-                                          ],
-                                          stops: [ .2,.6, .8],
-                                        ).createShader(bounds),
-                                        child: Icon(
-                                          Icons.check_circle_outline,
-                                          size: MediaQuery.of(context)
-                                                      .orientation ==
-                                                  Orientation.portrait
-                                              ? 35
-                                              : 45,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                )
-                              : const SizedBox())));
-            }));
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+      child: GridView.builder(
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        scrollDirection: Axis.vertical,
+        itemCount: widget.list.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: MediaQuery.of(context).orientation == Orientation.portrait ? 2 : 3,
+          childAspectRatio: 2, 
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                widget.selectedThumbnail = index;
+              });
+            },
+            child: Card(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: widget.list[index],
+                    fit: BoxFit.cover
+                  ),
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                height: 200,
+                width: 200,
+                child: widget.selectedThumbnail == index ? BlurredBox(
+                  borderRadius: BorderRadius.circular(10),
+                  sigma: 2,
+                  child: Container(
+                    decoration: const BoxDecoration(color: Color.fromARGB(100, 0, 0, 0)),
+                    child: Center(
+                      child: ShaderMask(
+                        blendMode: BlendMode.srcIn,
+                        shaderCallback: (Rect bounds) => const LinearGradient(
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                          colors: [
+                            Colors.lightBlue,
+                            Colors.pink,
+                            Colors.yellow,
+                          ],
+                          stops: [ .2,.6, .8],
+                        ).createShader(bounds),
+                        child: Icon(
+                          Icons.check_circle_outline,
+                          size: MediaQuery.of(context).orientation == Orientation.portrait ? 35 : 45,
+                        ),
+                      ),
+                    ),
+                  ),
+                ) : const SizedBox()
+              )
+            )
+          );
+        }
+      )
+    );
   }
 }
